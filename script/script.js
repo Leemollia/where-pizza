be822cf215bf186a3fe06a474910c7152b7a4303
Leemollia <leemollia@gmail.com>
2022-02-26 00:48:03 +0300

    header changed (add sticky), added popup

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
 
 
-let headerSticky = document.querySelector('.header-sticky')
+const headerSticky = document.querySelector('.header-sticky')
+const headerStickyLinks = document.querySelectorAll('.sticky__links')
 
-function showHeaderSticky() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        headerSticky.style.display = 'none'
+window.onscroll = function showHeaderSticky() {
+    if (document.documentElement.scrollTop > 180) {
+        headerStickyLinks[0].style.display = 'flex'
+        // headerStickyLinks.style.opacity = '0'
     } else {
-    headerSticky.style.display = 'flex'}
+        headerStickyLinks[0].style.display = 'none'}
+        // headerStickyLinks.style.opacity = '1'}
 };
 
-showHeaderSticky()
+const popupOther = document.querySelector('.popup-other')
+const openPopupOther = document.querySelector('.open-popupother')
 
-console.log(document.body)
+openPopupOther.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupOther.classList.toggle('open');
+})
