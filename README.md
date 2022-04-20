36526a7d2fec25071bf9e898c61fb4331371ac48
Пользователь <leemollia@gmail.com>
2022-04-20 13:02:11 +0300

    Правки

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Where Pizza<h1/>
+<div>Проект сайта доставки пиццы<div>
+Макет сайта: https://www.figma.com/file/TlqtV3siIjSlVAScqH5YxM/
---
+++ script/script.js
@@ -0,0 +1,53 @@
+
+
+const headerSticky = document.querySelector('.header-sticky')
+const headerStickyLinks = document.querySelectorAll('.sticky__links')
+
+window.onscroll = function showHeaderSticky() {
+    if (document.documentElement.scrollTop > 180) {
+        headerStickyLinks[0].style.display = 'flex'
+    } else {
+        headerStickyLinks[0].style.display = 'none'
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const allButtonFilter = document.querySelectorAll('.button-filter')
+    const allButtonSelect = document.querySelectorAll('.button-select')
+
+    allButtonFilter.forEach(button => {
+        button.setAttribute('uk-toggle', 'target: #filter-menu')
+
+        button.addEventListener('click', () => {
+            button.setAttribute('uk-toggle', 'target: #filter-menu')
+        })
+    })
+
+    allButtonSelect.forEach(button => {
+        button.setAttribute('uk-toggle', 'target: #modal-menu')
+
+        button.addEventListener('click', () => {
+            button.setAttribute('uk-toggle', 'target: #modal-menu')
+        })
+    })
+})
+
+
+const allButtonBorder = document.querySelectorAll('.button-border')
+
+allButtonBorder.forEach(button => {
+
+    button.addEventListener('click', () => {
+        button.classList.toggle('button-border-focus')
+    })
+})
+
+const allButtonModalSelect = document.querySelectorAll('.button-modal-select')
+
+allButtonModalSelect.forEach(button => {
+
+    button.addEventListener('click', () => {
+        button.classList.toggle('border-select')
+    })
+})
