79031e06d2cdf0114d64eaafc6dd40ef7f8f1036
Leemollia <leemollia@gmail.com>
2022-05-16 18:44:29 +0300

    Правки

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ window.onscroll = function showHeaderSticky() {
   }
 };
 
-const database = fetch("https://github.com/Leemollia/where-pizza/blob/a92ec15e43859ae27ed8e121a965d751d63de1cb/database.json")
+const database = fetch("../database.json")
   .then((res) => res.json())
   .then((out) => {
     //console.log(out);
